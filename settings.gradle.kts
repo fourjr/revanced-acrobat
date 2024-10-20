8856d27126ac888e35cd59cf192e615af24fb19b
Yee Jia Rong <28086837+fourjr@users.noreply.github.com>
2024-10-20 23:30:58 +0800

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,19 @@
-rootProject.name = "revanced-acrobat"
+rootProject.name = "revanced-patches-template"
 
 pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
-        mavenCentral()
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/revanced/registry")
-//            url = uri("https://maven.pkg.github.com/OWNER/REPOSITORY")
             credentials {
-                username = providers.gradleProperty("gpr.user").orNull ?: "username"
-                password = providers.gradleProperty("gpr.key").orNull ?: "token"
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
             }
         }
     }
 }
-
 
 plugins {
     id("app.revanced.patches") version "1.0.0-dev.5"

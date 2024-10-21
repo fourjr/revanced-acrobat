06cc7891e85b8129ac638631195c9e793258dac9
Yee Jia Rong <28086837+fourjr@users.noreply.github.com>
2024-10-21 17:24:32 +0800

    oops

--- src/main/kotlin/app/revanced/patches/acrobat/UnlockPremiumPatch.kt
+++ src/main/kotlin/app/revanced/patches/acrobat/UnlockPremiumPatch.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.acrobat.premium.fingerprints.HasPurchasedFingerprint
+import app.revanced.patches.acrobat.fingerprints.HasPurchasedFingerprint
 
 @Patch(
     name = "Unlock premium for acrobat",

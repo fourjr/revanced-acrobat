e560d1b0c42df9d9e6878145f1decf74cea9ccc4
Yee Jia Rong <28086837+fourjr@users.noreply.github.com>
2024-10-21 18:32:46 +0800

    revert to tooling

---
+++ src/main/kotlin/app/revanced/patches/build.gradle.kts
@@ -0,0 +1,13 @@
+group = "app.revanced"
+
+patches {
+    about {
+        name = "Revanced Acrobat"
+        description = "Patches for acrobat revanced"
+        source = "git@github.com:fourjr/revanced-acrobat.git"
+        author = "fourjr"
+        contact = "contact@revanced.app"
+        website = "https://revanced.app"
+        license = "GNU General Public License v3.0"
+    }
+}

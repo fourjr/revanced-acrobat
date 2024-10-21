4e7415afff14899e2681c24d7dec262f153993fa
Yee Jia Rong <28086837+fourjr@users.noreply.github.com>
2024-10-21 19:08:03 +0800

    upgrade to patcher-v20

--- patches/src/main/kotlin/app/revanced/patches/acrobat/premium/UnlockPremiumPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/acrobat/premium/UnlockPremiumPatch.kt
@@ -1,23 +1,18 @@
 package app.revanced.patches.acrobat.premium
 
-import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
-import app.revanced.patcher.patch.BytecodePatch
-import app.revanced.patcher.patch.annotation.CompatiblePackage
-import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.acrobat.premium.fingerprints.HasPurchasedFingerprint
+import app.revanced.patcher.patch.bytecodePatch
 
-@Patch(
-    name = "Unlock premium for acrobat",
-    compatiblePackages = [CompatiblePackage("com.adobe.reader")]
-)
 @Suppress("unused")
-object UnlockPremiumPatch : BytecodePatch(
-    setOf(HasPurchasedFingerprint)
-){
-    override fun execute(context: BytecodeContext) {
-        // Set hasPremium = true using smaliInstructions
-        HasPurchasedFingerprint.result?.mutableMethod?.replaceInstruction(2, "const/4 v2, 0x1")
-            ?: throw IllegalArgumentException()
+val unlockPremiumPatch = bytecodePatch(
+    name = "Unlock premium",
+) {
+    compatibleWith("com.adobe.reader")
+
+    val hasPurchasedMatch by hasPurchasedFingerprint()
+
+    execute {
+        // Set hasPremium = true.
+        hasPurchasedMatch.mutableMethod.replaceInstruction(2, "const/4 v2, 0x1")
     }
 }
